384e02e18b08eca564456054f0bb8435d1a571e9
Mehmet Akif PAŞA <m.akif.pasa@gmail.com>
2020-09-30 14:10:17 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,4 @@ INSERT INTO student (name, department, updated_by, updated_on)
 VALUES ('İpek ÇİÇEK', 'Nurse', 'Teck Pro', now());
 INSERT INTO student (name, department, updated_by, updated_on)
 VALUES ('Akif Mehmet PAŞA', 'Mechanical Engineer', 'Akif', now());
-INSERT INTO user values (2, '$2a$10$Oy2LqReubARmgTW6/oXDDe8sP1lk32Q5x5KG.TVjfQl2.YKcVKmeW', 'user2');
+INSERT INTO user values (2, '$2a$10$Oy2LqReubARmgTW6/oXDDe8sP1lk32Q5x5KG.TVjfQl2.YKcVKmeW', 'user2'); # Username: user2 Password: abcd
